a9059329cdb7220845d20f6aba6cbd1909c8c5a4
Muhammad <mh5154@nyu.edu>
2019-09-15 18:36:10 -0700

    Adding Clion source files

---
+++ cmake-build-debug/CMakeFiles/Application_Security.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Application_Security.dir/main.c.o"
+  "Application_Security.pdb"
+  "Application_Security"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Application_Security.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
