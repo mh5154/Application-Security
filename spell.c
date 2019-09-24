b006f155cd55f56fe23bf8ecd89036edf9d13617
Muhammad <mh5154@nyu.edu>
2019-09-24 01:15:16 -0700

    spell.c Rev2

--- spell.c
+++ spell.c
@@ -1,14 +1,55 @@
-#include <stdio.h>
+#include "stdio.h"
+#include "stdlib.h"
 #include "dictionary.h"
+#include "string.h"
+
+//test
+
+int check_words(FILE* fp, hashmap_t hashtable[], char * misspelled[])
+{
+    int num_misspelled = 0;
+    while(fp != EOF)
+    {
+        char* readLine;
+        fscanf(fp, "%[^\n]", readLine);
+        char* temp = strtok(readLine, "'");
+        if(check_word(temp, hashtable) == false)
+        {
+            strcat(misspelled, temp);
+            num_misspelled++;
+        }
+    }
+    return num_misspelled;
+}
 
 
-int check_words(FILE* fp, hashmap_t hashtable[], char * misspelled[]);
 
 
 
+bool check_word(const char* word, hashmap_t hashtable[])
+{
+    int bucket = 0;
+    bucket = hash_function(word);
+    hashmap_t = hashtable[bucket];
+    while(hashmap_t != NULL)
+    {
+        if(word == hashmap_t->word)
+            return true;
+        hashmap_t = hashmap_t->next;
+    }
 
+    bucket = hash_function(word);
+    hashmap_t = hashtable[bucket];
+    while(hashmap_t != NULL)
+    {
+        if(strlwr(word) == hashmap_t->word)
+            return true;
+        hashmap_t  = hashmap_t->next;
+    }
 
-bool check_word(const char* word, hashmap_t hashtable[]);
+    return false;
+
+}
 
 
 
@@ -23,22 +64,61 @@ bool load_dictionary(const char* dictionary_file, hashmap_t hashtable[])
     }
 
     FILE *fp = fopen("wordlist.txt", "r");
-
     if(fp == NULL)
     {
         return false;
     }
     else
     {
-        char newword;
-        while(fscanf(fp,"%[^\n]", newword) != EOF)
-            getle
+        char *readWord[20];
+        int bucket = 0;
+        while(fp != EOF)
+        {
+            fscanf(fp, "%[^\n]", readWord);
+
+
+
+            struct node* newNode = NULL;
+            newNode = (struct node*)malloc(sizeof(struct node));
+            newNode->next = NULL;
+            newNode->word = readWord;
+
+            bucket = hash_function(readWord);
+
+            if(hashtable[bucket] == NULL)
+                hashtable[bucket] = newNode;
+            else
+            {
+                newNode->next = hashtable[bucket];
+                hashtable[bucket] = newNode;
+            }
+
+            /*node* temp = new node;
+            node* temp2;
+            temp->word = readWord;
+            temp->next = NULL;
+
+            hashtable[bucket]->next
+            if(hashtable[bucket]->next == NULL)
+            {
+                hashtable[bucket]->next = temp;
+            }
+            else
+            {
+                temp2 = hashtable[bucket]->next;
+                while(temp2->next != NULL)
+                    temp2 = temp2->next;
+                temp2->next = temp;
+                temp2 = NULL;
+            }*/
+        }
+        fclose(fp);
 
     }
 }
 
-
+/*
 int main() {
     printf("Hello, World!\n");
     return 0;
-}
+}*/
