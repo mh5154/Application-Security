389821e658db781fb69a28aa74a4f43fdf916871
Muhammad <mh5154@nyu.edu>
2019-09-24 00:35:33 -0700

    add

--- spell.c
+++ spell.c
@@ -1,4 +1,42 @@
 #include <stdio.h>
+#include "dictionary.h"
+
+
+int check_words(FILE* fp, hashmap_t hashtable[], char * misspelled[]);
+
+
+
+
+
+bool check_word(const char* word, hashmap_t hashtable[]);
+
+
+
+
+
+
+bool load_dictionary(const char* dictionary_file, hashmap_t hashtable[])
+{
+    for(int i = 0; i < 1000; i++)
+    {
+        hashtable[i] = NULL;
+    }
+
+    FILE *fp = fopen("wordlist.txt", "r");
+
+    if(fp == NULL)
+    {
+        return false;
+    }
+    else
+    {
+        char newword;
+        while(fscanf(fp,"%[^\n]", newword) != EOF)
+            getle
+
+    }
+}
+
 
 int main() {
     printf("Hello, World!\n");
